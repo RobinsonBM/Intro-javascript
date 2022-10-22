086e14efaee97d30e0f8d60183cef29c06f73039
Robinson Betancur Marin <robinsonb@iadb.org>
2022-10-22 12:15:07 -0500

    const and let (variables)

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+const nombre = 'Robinson';
+const apellido = 'Betancur';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var NO se debe usar....
+if( true ) {
+    const nombre = 'Peter'
+    // let valorDado = 6;
+    console.log(nombre);
+}
+
+console.log(valorDado);
